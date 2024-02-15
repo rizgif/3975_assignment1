781929a761537c67bc024f8c5499d15ed3327ff7
Diane Choi <ekska21@gmail.com>
2024-02-15 15:46:23 -0800

    added login and register buttons

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include 'inc_header.php';?>
+
+<div class="container text-center">
+  <button class="btn btn-primary" onclick="window.location.href = 'login.php'">Login</button>
+  <button class="btn btn-success" onclick="window.location.href = 'register.php'">Register</button>
+</div>
+
+<footer class="footer bg-light mt-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <hr>
+        <div class="text-right"> 
+          <p class="mb-0">Riz Nur Saidy</p>
+          <p class="mb-0">Diane Choi</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<?php include 'inc_footer.php';?>
