b9accf7c3f7d7d32acb1c315ef11bc13cfd551fa
Diane Choi <ekska21@gmail.com>
2024-02-15 15:48:10 -0800

    added login and register forms

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php include 'inc_header.php'; ?>
+
+<h1 class="text-center">Sign up</h1>
+<form action="" method="post">
+  <div class="form-group">
+    <label for="email">Enter your Email:</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Enter your password:</label>
+    <input type="password" class="form-control" id="pwd" name="pwd">
+  </div>
+  <button type="submit" class="btn btn-default">Sign Up</button>
+
+  <?php include 'inc_footer.php'; ?>
